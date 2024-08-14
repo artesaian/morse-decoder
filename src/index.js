7fa54cbb10cc8b17cf4354e19d840538fb385fd0
Artur Yesaian <artesayan2@gmail.com>
2024-08-14 16:49:34 +0200

    fix error

--- src/index.js
+++ src/index.js
@@ -38,9 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    if (!expr) return '';
+
+    let decodedMessage = ''; 
+
+    for (let i = 0; i < expr.length; i += 10) {
+        const encodedLetter = expr.slice(i, i + 10); 
+
+        if (encodedLetter === '**********') {
+            decodedMessage += ' ';
+        } else {
+            const morseSymbol = encodedLetter
+                .replace(/10/g, '.')
+                .replace(/11/g, '-')
+                .replace(/0/g, '');
+            decodedMessage += MORSE_TABLE[morseSymbol] || '';
+        }
+    }
+
+    return decodedMessage; 
 }
 
 module.exports = {
     decode
-}
+};
